336006c4d7c48feaf75ad3f1a298e87ea7848637
matthiebl <z5362740@unsw.edu.au>
2022-07-02 17:11:29 +1000

    Make weaverbot perform more like a command line tool
    Add a solve (-s) and next (-n) option to utilise different functions

--- weaverbot.py
+++ weaverbot.py
@@ -24,7 +24,7 @@ def solve(word, end, lst, collection, length=7):
             newLst.append(poss)
             solve(poss, end, newLst, collection, length)
 
-def main():
+def solver():
     leng = 7
     if len(sys.argv) >= 2:
         leng = int(sys.argv[1])
@@ -48,4 +48,32 @@ def main():
             print(f'\nOne of the best answers is: {" -> ".join(every[-1])}')
         else:
             print(f'\nThere are no solutions of length {leng} or less')
+
+def usage():
+    print(f'usage: weaverbot')
+
+def main():
+    if len(sys.argv) == 4 and sys.argv[1] in ['-s', '--solve']:
+        start = sys.argv[2]
+        end = sys.argv[3]
+        if start not in wordDict:
+            print(f"{sys.argv[0]}: error: '{start}' is not a valid word in the dictionary")
+            sys.exit(1)
+        if end not in wordDict:
+            print(f"{sys.argv[0]}: error: '{end}' is not a valid word in the dictionary")
+            sys.exit(1)
+        every = []
+        solve(start, end, [], every, length=7)
+        for e in sorted(every, key=len, reverse=True):
+            print(e)
+    elif len(sys.argv) == 3 and sys.argv[1] in ['-n', '--next']:
+        word = sys.argv[2]
+        if word not in wordDict:
+            print(f"{sys.argv[0]}: error: '{word}' is not a valid word in the dictionary")
+            sys.exit(1)
+        print(f"{word}: {', '.join(wordDict[word])}")
+    else:
+        usage()
+        sys.exit(1)
+
 main()
